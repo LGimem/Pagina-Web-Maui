d15e250251d0bd9cf5d89b79bd065d2e8d83dfe4
LGimem <laragimenezm@gmail.com>
2022-10-02 21:56:02 -0300

    nuevas modificaciones del telefono

---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+var pais = document.getElementById('pais');
+var telf = document.getElementById('telf');
+
+console.log('Archivo Js cargado')
+
+pais.onchange = function(e) {
+	telf.value = this.value;
+	if((this.value).trim() != '') {
+		telf.disabled = false;
+	} else {
+		telf.disabled = true
+	}
+}
+ 
+telf.onkeyup = function(e) {
+	var nums_v = this.value.match(/\d+/g);
+	if (nums_v != null) {
+		this.value = '+'+((nums_v).toString().replace(/\,/, ''));
+	} else {
+		this.value = pais.value;
+	}
+}
+
+
+
+
+  
